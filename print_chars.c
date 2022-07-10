a50e225db91b9d723b86b8fe2ebb9370902a34d0
Atusiimire <mutatinamoureen@gmail.com>
2022-07-10 14:02:24 -0700

    specifier c s

---
+++ print_chars.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_c - prints a char
+ * @c: char to print
+ * Return: always 1
+ */
+
+int print_c(va_list c)
+{
+char ch = (char)va_arg(c, int);
+_putchar(ch);
+return (1);
+}
+/**
+ * print_s - prints a string
+ * @s: string to print
+* Return: number of chars printed
+ */
+int print_s(va_list s)
+{
+int count;
+char *str = va_arg(s, char *);
+if (str == NULL)
+str = "(null)";
+for (count = 0; str[count]; count++)
+{
+_putchar(str[count]);
+}
+return (count);
+}
