457fa282983d3a99db3943476bfccd71ee618b39
Atusiimire <mutatinamoureen@gmail.com>
2022-07-11 00:01:46 -0700

    header file

--- main.h
+++ main.h
@@ -1,38 +1,31 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef _MAINN_H_
+#define _MAIN_H_
 #include <stdarg.h>
-#include <stddef.h>
-#include <stdlib.h>
 
 /**
- * struct structprint - structure containing
- * @q: the location and method to translate data to characters.
- * @u: print function for specific type.
- *
- * Return: int
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-typedef struct structprint
-{
-	char *q;
-	int (*u)(char *format, va_list);
-} structype;
 
-int _putchar(char ch);
-int _puts(char *string);
-int printc(char *format, va_list);
-int printstr(char *format, va_list);
-int (*driver(char *format))(char *format, va_list);
-int _printf(char *format, ...);
-int printint(char *format, va_list pa);
-int integer(int number);
-int contadordigit(int number);
-int _abs(int number);
-int printpercent(char *format, va_list pa);
-int printhex(char *format, va_list);
-int printHEX(char *format, va_list);
-int printocta(char *format, va_list);
-int print_unsign(char *format, va_list);
+typedef struct print
+{
+char *t;
+int (*f)(va_list);
+} print_t;
+int _putchar(char c);
+int _printf(const char *format, ...);
 int print_c(va_list c);
 int print_s(va_list s);
-#endif
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+#endif  /* _MAINN_H */
